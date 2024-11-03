09088a373d55b86cec53ddaa591317af9b2c86f5
Thuan Do PC <thuando.contact@gmail.com>
2024-11-03 14:11:17 +0700

    add function drive to DAL class update password

--- BUS/userBUS.py
+++ BUS/userBUS.py
@@ -7,7 +7,7 @@ class userBUS:
 
     def login(self, userDTO):
         #call DAL method to check login
-        print('ok')
+        print('calling login bus....')
         return self.userDAL.check_user(userDTO.username, userDTO.password)
     
     def register(self, user, email, fullname):
@@ -16,6 +16,10 @@ class userBUS:
             raise ValueError("Email is existed in database.") #if existed    
         else:
             self.userDAL.add_user(user.username, fullname, user.password, email) 
+
+
+    def update_pwd(self, email, new_pwd):
+        return self.userDAL.update_pwd(email, new_pwd)
 
     
     
