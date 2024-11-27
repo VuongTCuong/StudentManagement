e5d8417976561da371359812f94f10d07677a7e6
TieuCuong <vuong.tieucuong@gmail.com>
2024-11-27 10:43:43 +0700

    add saving login user

--- GUI/Mailbox.py
+++ GUI/Mailbox.py
@@ -1,1 +1,9 @@
 import customtkinter as ctk
+
+class Mailbox:
+    def __init__(self):
+        self.root = ctk.CTk()
+        self.root.geometry('600x400')
+        self.root.mainloop()
+
+Mailbox()
